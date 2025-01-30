f766bb5d411172c2a26ebb79c121984cb0ba78bf
Rovic Castro <193394253+roviccastro@users.noreply.github.com>
2025-01-30 23:02:50 +0800

    Feat: Create operator func to call operation

--- script.js
+++ script.js
@@ -1,12 +1,51 @@
+let numOne = 0;
+let numTwo = 0;
+let operator = '';
+
+function operate(){
+  numOne = window.prompt('Enter Number:', '0');
+  operator = window.prompt('Enter Operator:', '+');
+  numTwo = window.prompt('Enter Number:', '0');
+  
+  switch(operator){
+   
+    default:
+    case '+':{
+      add(numOne, numTwo);
+      break;
+    }
+
+    case '-':{
+      subtract(numOne, numTwo);
+      break;
+    }
+
+    case '*':{
+      multiply(numOne, numTwo);
+      break;
+    }
+
+    case '/':{
+      divide(numOne, numTwo);
+      break;
+    }
+
+  }
+
+}
+
 function add(a, b){
+  console.log(+a + +b)
   return +a + +b;
 }
 
 function subtract(a, b){
+  console.log(+a - +b)
   return +a - +b;
 }
 
 function multiply(a, b){
+  console.log(+a * +b)
   return +a * +b;
 }
 
@@ -14,5 +53,8 @@ function divide(a, b){
   if(!a | !b){
     return `Can't divide by 0`
   }
+  console.log(+a / +b)
   return +a / +b;
 }
+
+operate()
